23e8d570ce1c01aa58de985a152719a261dcece9
Collin_Stubbs <collin.stubbs@greenwoodcollege.com>
2019-11-06 12:00:54 -0500

    Task 4 complete

--- Nov6Tasks.py
+++ Nov6Tasks.py
@@ -1,3 +1,5 @@
+import random
+
 def task_1():
     print("Hello World!")
 
@@ -8,4 +10,14 @@ def task_3(data):
     data = data.upper()
     return data
 
-print(task_3("hello"))
+def task_4(num1, num2, num3):
+    rand1 = random.randint(2,4)
+    rand2 = random.randint(2,4)
+    rand3 = random.randint(2,4)
+
+    if num1 == rand1 and num2 == rand2 and num3 == rand3:
+        return "You won!"
+    else:
+        return "You lost! Loser"
+
+text = task_4(2, 3, 4)
